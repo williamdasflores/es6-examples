00ff007ca273c528b9f32d04a48624cc32923e6e
William da Silva Flores <william.da.silva.flores@ibm.com>
2019-07-17 09:27:29 -0300

    alterando nome de parametro

--- ES6/classes/prototype_example.js
+++ ES6/classes/prototype_example.js
@@ -3,8 +3,8 @@ function Person(person) {
 }
 
 //Adding a method
-Person.prototype.calcAge = function(year) {
-    return year - 1990;
+Person.prototype.calcAge = function(currentYear) {
+    return currentYear - 1990;
 }
 
 const person = new Person({ name: 'William' });
